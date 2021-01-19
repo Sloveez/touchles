dc13743d4214a6322f368e7fb81b6f0275db2764
Michael Sloves <sloveez@gmail.com>
2021-01-18 20:24:33 -0500

    Custom theme rename (#4)
    * Firebase and Auth Provider (Google)
    
    * Save user to Firebase users collection
    
    * Added Chakra UI and created logo icon
    
    * Changed name of theme to customTheme

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from "next/head";
 import { Flex, Heading, Button, Text, Code, Icon } from "@chakra-ui/react";
+
 import { useAuth } from "@/lib/auth";
-import { LogoIcon } from "@/styles/logoicon";
+import { LogoIcon } from "../styles/logoicon";
 
 const Home = () => {
   const auth = useAuth();
--- styles/theme.js
+++ styles/theme.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { theme as chakraTheme } from "@chakra-ui/react";
 
-const theme = {
+const customTheme = {
   ...chakraTheme,
   fonts: {
     ...chakraTheme.fonts,
@@ -14,4 +14,4 @@ const theme = {
   },
 };
 
-export default theme;
+export default customTheme;
