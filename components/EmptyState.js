943778faf1ffe8bac8608d412cc32d675b587eac
Michael Sloves <sloveez@gmail.com>
2021-01-20 08:33:14 -0500

    Added locations api (#6)
    * Firebase and Auth Provider (Google)
    
    * Save user to Firebase users collection
    
    * Added Chakra UI and created logo icon
    
    * Changed name of theme to customTheme
    
    * Created Dashboard shell and create site dialogs.
    
    * Added location stable and locations api.

--- components/EmptyState.js
+++ components/EmptyState.js
@@ -5,23 +5,21 @@ import DashboardShell from "./DashboardShell";
 import AddLocationModal from "./AddLocationModal";
 
 const EmptyState = () => (
-  <DashboardShell>
-    <Flex
-      backgroundColor="white"
-      width="100%"
-      p={16}
-      borderRadius="4px"
-      justify="center"
-      direction="column"
-      align="center"
-    >
-      <Heading size="md" mb={2}>
-        No locations added.
-      </Heading>
-      <Text mb={4}>Welcome, add your first location.</Text>
-      <AddLocationModal />
-    </Flex>
-  </DashboardShell>
+  <Flex
+    backgroundColor="white"
+    width="100%"
+    p={16}
+    borderRadius="4px"
+    justify="center"
+    direction="column"
+    align="center"
+  >
+    <Heading size="md" mb={2}>
+      No locations added.
+    </Heading>
+    <Text mb={4}>Welcome, add your first location.</Text>
+    <AddLocationModal>Add First Location</AddLocationModal>
+  </Flex>
 );
 
 export default EmptyState;
